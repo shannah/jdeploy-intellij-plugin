a91955a035c3ca6326db3722d8f1e7d0b80435fc
Steve Hannah <steve@weblite.ca>
2023-12-29 12:08:20 -0800

    Changed target java version to 11 to support older versions of intellij

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,11 @@ dependencies {
 tasks {
   // Set the JVM compatibility versions
   withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
   }
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "11"
   }
 
   patchPluginXml {
